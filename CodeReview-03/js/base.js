f0066c3467d773e2bfbaf9588f744ffeb2847c91
aaa <55081213+defaziofederico@users.noreply.github.com>
2019-09-28 10:35:10 +0200

    Before JS

---
+++ CodeReview-03/js/base.js
@@ -0,0 +1,45 @@
+ // //var age =document.getElementById["sum"]["age"].value;
+ //            var hp = document.forms["sum"]["hp"].value;
+
+ //            var sum = Number(age) + Number(hp);
+  
+ //            var res = document.getElementById('res');
+ //            var res.innerHTML = '<h2>The sum of the numbers is ' + res + ' </h2>';
+
+            
+ //            console.log(a+b);//
+
+	
+
+// function insCalc() {
+// 	var hp=100;
+// 	var age=50;
+
+// 	var sum=((hp*100)/(age+50));
+// 	document.write(sum)
+
+//         }
+
+//     insCalc()
+//     
+
+// function insCalc () {
+// var hp=100;
+// var age=50;
+// var country=austria;
+// var country1=hungary;
+// var country2=greece;
+
+// if (country="austria") {
+//  var res=(hp*100/(age+50));
+//     console.log(res1);
+// } else if (country1="hungary") {
+// 	var res1=(hp*120/(age+100));
+// 	console.log(res1);
+// } else if (country2="greece") {
+// 	 var res2=(hp*150/(age+3(+50)));
+// 	 console.log(res2);
+// }
+
+
+// insCalc ();
